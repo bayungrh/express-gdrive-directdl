975549c8fabe733f0b1aa40a4464eca491e9b89c
MuhBayu <bnugraha00@hotmail.com>
2019-02-04 17:03:09 +0700

    fix route code

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -17,7 +17,7 @@ exports.index = (req, res) => {
 exports.api = async (req, res) => {
 	const input = req.query;
 	axios.post(`https://drive.google.com/uc?id=${input.id}&authuser=0&export=download`, {}, {
-		headers: {'accept-encoding': 'gzip, deflate, br', 'content-length': 0, 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8', 'origin': 'https://drive.google.com', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36','x-client-data': 'CKG1yQEIkbbJAQiitskBCMS2yQEIqZ3KAQioo8oBGLeYygE=', 'x-drive-first-party': 'DriveWebUi', 'x-json-requested': 'true,' }
+		headers: {'accept-encoding': 'gzip, deflate, br', 'content-length': 0, 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8', 'origin': 'https://drive.google.com', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36','x-client-data': 'CKG1yQEIkbbJAQiitskBCMS2yQEIqZ3KAQioo8oBGLeYygE=', 'x-drive-first-party': 'DriveWebUi', 'x-json-requested': 'true' }
 	}).then(data => {
 		var result = data.data;
 		result = result.slice(4);
--- routes/web.js
+++ routes/web.js
@@ -4,13 +4,11 @@ const MainController = require('../controllers/main.controller');
 const response = require('../helpers/response');
 
 module.exports = (app, path) => {
-    app.route('/').get(MainController.index);
-    // user routes
-    app.get('/api', MainController.api)
+	app.get('/', MainController.index);
+	app.get('/api', MainController.api);
 	app.get('*', function(req, res, err){
 		return response.json({
 			code:404, message:'Not found'
 		}, res);
 	});
-
 };
